6d76e1c83212337537a2ecbb82f5f1633ad3fa55
Taichi Soma <izewfktvy533zjmn@gmail.com>
2020-03-10 08:40:09 +0900

    時間の範囲指定機能の追加

--- service/api/routes/v0/temperature.js
+++ service/api/routes/v0/temperature.js
@@ -9,7 +9,8 @@ const con_db  = mysql.createConnection({
     host: 'localhost',
     user: 'leaf_eye',
     password: 'leaf_eye',
-    database: 'leaf_eye'
+    database: 'leaf_eye',
+    timezone: 'jst'
 });
 
 con_db.connect();
@@ -36,6 +37,68 @@ router.get('/', function(req, res) {
     });
 
 });
+
+router.get(/date/, function(req, res) {
+    var parameters = req.path.split('/');
+    console.log(parameters);
+    
+    if (parameters.length == 5 || parameters.length == 8) {
+        var startDate   = parameters[2];
+        var endDate     = parameters[3];
+        var detailLevel = parameters[4];
+
+        if (endDate == '1d') {
+            endDate = startDate;
+        }
+
+        switch (detailLevel) {
+            case '1sec':
+                console.log("detailLevel:", detailLevel);
+                break;
+
+            case '1min':
+                console.log("detailLevel:", detailLevel);
+                break;
+        }
+
+    }
+    else {
+        // query error handle
+    }
+
+    if (parameters.length == 5) {
+        console.log(startDate, endDate, detailLevel);
+        
+        var query = 'select timestamp, temperature from data where timestamp between \'' + startDate + ' 00:00:00\' and \'' + endDate + ' 23:59:59\';';
+    }
+    else if (parameters.length == 8) {
+        let startTime = parameters[6];
+        let endTime   = parameters[7];
+        var query = 'select timestamp, temperature from data where timestamp between \'' + startDate + ' ' + startTime + '\' and \'' + endDate + ' ' + endTime + '\';';
+        console.log(query);
+    }
+
+    con_db.query(query, (err, rows, fields) => {
+        if (err) {
+            console.log('err: ', err);
+        }
+
+        array = [];
+
+        for (i in rows) {
+            var timestamp = rows[i].timestamp;
+            var row = {timestamp: timestamp, temperature: rows[i].temperature};
+            array.push(row);
+        }
+
+        var data = {"temperature": array};
+        res.header('content-type', 'application/json; charset=utf-8');
+        res.send(JSON.stringify(data, null, 1));
+    });
+
+
+});
+
 
 module.exports = router;
 
